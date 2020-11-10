599b492eb34b6a50a4bf31c17479a70373c9c236
ovacia345 <nielsvann@hotmail.nl>
2020-11-10 12:46:31 +0100

    Final code cleaning

--- fitted_network.R
+++ fitted_network.R
@@ -1,51 +1,64 @@
+# @author Niels van Nistelrooij
+# @author Victoria Bosch
 library(dagitty)
 library(bnlearn)
 library(lavaan)
 library(lavaanPlot)
-library(ppcor)
 library(pracma)
 library(ggplot2)
+library(ggpubr)
 
-# full Bayesian network is specified below
+# full Bayesian network is specified below using dagitty package
 g <- dagitty(paste('dag {',
     ####################################
     #            Variables             #
-    ####################################                   
+    ####################################
     # weather variables
-    'atemp [pos="-0.421,-0.331"]
-    casual [pos="-0.412,-0.304"]
-    daylength [pos="-0.422,-0.316"]
-    gasprice [pos="-0.422,-0.308"]
-    holiday [pos="-0.403,-0.301"]
+    'weathersit [pos="-0.402,-0.329"]
     hum [pos="-0.412,-0.329"]
-    instant [pos="-0.422,-0.297"]
-    rallyprotest [pos="-0.402,-0.308"]
-    registered [pos="-0.412,-0.321"]
-    season [pos="-0.428,-0.312"]
-    temp [pos="-0.421,-0.324"]
-    weathersit [pos="-0.402,-0.329"]
     windspeed [pos="-0.412,-0.337"]
-    workingday [pos="-0.402,-0.319"]',
-  
+    temp [pos="-0.421,-0.324"]
+    atemp [pos="-0.421,-0.331"]',
+    
+    # day variables
+    'season [pos="-0.428,-0.312"]
+    day [pos="-0.422,-0.297"]
+    workingday [pos="-0.402,-0.319"]
+    holiday [pos="-0.403,-0.301"]',
+    
+    # extra variables
+    'daylength [pos="-0.422,-0.316"]
+    gasprice [pos="-0.422,-0.308"]
+    rallyprotest [pos="-0.402,-0.308"]',
+    
+    # outcome variables
+    'casual [pos="-0.412,-0.304"]
+    registered [pos="-0.412,-0.321"]',
+    
     ####################################
-    #            Relations             #
+    #            Connections           #
     ####################################
+    # weather relations
     'season -> daylength -> temp -> atemp
-    weathersit -> {windspeed -> hum} -> atemp
+    weathersit -> {windspeed -> hum} -> atemp',
     
-    {season instant} -> gasprice
+    # gas price relations
+    '{season day} -> gasprice',
     
-    {instant atemp workingday weathersit gasprice daylength holiday rallyprotest} -> casual
-    {instant atemp workingday weathersit gasprice daylength} -> registered
-}'))
-plot(g)
+    # outcome relations
+    '{day atemp workingday weathersit gasprice daylength holiday rallyprotest} -> casual
+    {day atemp workingday weathersit gasprice daylength} -> registered',
+'}'))
+plot(g)  # plot Bayesian network
 
 
 
 # use hourly data as source
 hour.data = read.csv("hour.csv", header=TRUE)
+# determine indices of day starts
 day.starts <- hour.data$instant[!duplicated(hour.data$dteday)]
 
+# function to compute day length in Washington D.C.
 daylength <- function(days)
 {
   result <- NULL
@@ -60,14 +73,15 @@ daylength <- function(days)
         / 
           (cos(L * pi / 180) * cos(P))
       )
+    
     result <- c(result, D)
   }
   return(result)
 }
 
-
+# initialize dataframe for daily data set
 data <- data.frame(
-    instant=seq_along(day.starts),
+    day=seq_along(day.starts),
     season=hour.data[day.starts, "season"] %% 4 > 1,
     holiday=hour.data[day.starts, "holiday"] == 1,
     workingday=hour.data[day.starts, "weekday"] %% 6 != 0,
@@ -88,27 +102,27 @@ data <- data.frame(
 
 # aggregate over hours to get daily variables
 day.intervals <- c(day.starts, nrow(hour.data) + 1)
-for (I in data$instant)
+for (day in data$day)
 {
-    day.data <- hour.data[day.intervals[I]:(day.intervals[I + 1] - 1),]
+    day.data <- hour.data[day.intervals[day]:(day.intervals[day + 1] - 1),]
 
     if (sum(day.data$weathersit == 3 | day.data$weathersit == 4) >= 3)
     {
-        data$weathersit[I] <- 3
+        data$weathersit[day] <- 3
     } else if (sum(day.data$weathersit == 2 | day.data$weathersit == 3 | day.data$weathersit == 4) >= 3)
     {
-        data$weathersit[I] <- 2
+        data$weathersit[day] <- 2
     } else
     {
-        data$weathersit[I] <- 1
+        data$weathersit[day] <- 1
     }
     
-    data[I, "hum"] <- mean(day.data$hum)
-    data[I, "windspeed"] <- mean(day.data$windspeed)
-    data[I, "temp"] <- mean(day.data$temp)
-    data[I, "atemp"] <- mean(day.data$atemp)
-    data[I, "casual"] <- sum(day.data$casual)
-    data[I, "registered"] <- sum(day.data$registered)
+    data[day, "hum"] <- mean(day.data$hum)
+    data[day, "windspeed"] <- mean(day.data$windspeed)
+    data[day, "temp"] <- mean(day.data$temp)
+    data[day, "atemp"] <- mean(day.data$atemp)
+    data[day, "casual"] <- sum(day.data$casual)
+    data[day, "registered"] <- sum(day.data$registered)
 }
 
 # add weekly gasoline price
@@ -124,20 +138,20 @@ gasprice.per.week <- c(
     3.823, 3.86, 3.953, 3.907, 3.893, 3.911, 3.877, 3.787, 3.692, 3.677, 3.66,
     3.657, 3.637, 3.594, 3.545, 3.486, 3.471, 3.5
 )
-week.starts <- c(1, data$instant[hour.data[day.starts, "weekday"] == 1], 732)
-for (I in 1:(length(week.starts) - 1))
+week.starts <- c(1, data$day[hour.data[day.starts, "weekday"] == 1], 732)
+for (week in 1:(length(week.starts) - 1))
 {
-    data[week.starts[I]:(week.starts[I + 1] - 1), "gasprice"] <- gasprice.per.week[I]
+    data[week.starts[week]:(week.starts[week + 1] - 1), "gasprice"] <- gasprice.per.week[week]
 }
 
-# scale all variables to z-scores
+# scale all variables except the ordinal weather situation to z-scores
 numeric.data <- data.frame(lapply(subset(data, select=-weathersit), as.numeric))
 scaled.data <- data.frame(scale(numeric.data))
 
-# put ordinal variable weathersit back in data.frame
+# put ordinal weather situation back in dataframes
 data <- cbind(numeric.data, weathersit=data$weathersit)
 scaled.data <- cbind(scaled.data, weathersit=data$weathersit)
-scaled.data$atemp[595] <- -scaled.data$atemp[595]
+scaled.data$atemp[595] <- -scaled.data$atemp[595]  # reverse outlier
 
 
 
@@ -154,7 +168,7 @@ plotLocalTestResults(top.results, xlab="Partial correlation coeffcient (95% CI)"
 
 # fit data to SEM using lavaan package
 net <- toString(g, "lavaan")
-fit <- sem(net, sample.cov=M, sample.nobs=nrow(data))
+fit <- lavaan(net, sample.cov=M, sample.nobs=nrow(data), auto.var=TRUE)
 summary(fit, fit.measures=TRUE)
 
 # show SEM with coefficients
@@ -169,7 +183,7 @@ lavaanPlot(
 
 # predict with SEM using bnlearn package
 net <- model2network(toString(g, "bnlearn"))
-fit <- bn.fit(net, data)
+fit <- bn.fit(net, data)  # use non-scaled version of data for prediction
 
 # predict users for different values of workingday, holiday, and weathersit
 tbl <- expand.grid(workingday=c(1, 0), holiday=c(1, 0), weathersit=c(1, 2, 3))
@@ -178,47 +192,25 @@ tbl$casual <- predict(fit, node="casual", data=tbl, method="bayes-lw")
 tbl$registered <- predict(fit, node="registered", data=tbl, method="bayes-lw")
 tbl
 
-# predict users a year into the future
-tbl <- data.frame(instant=as.numeric((nrow(data) + 1):(nrow(data) + 365)))
-tbl$casual <- predict(fit, node="casual", data=tbl, method="bayes-lw")
+# predict registered users for different gas prices
+tbl <- data.frame(gasprice=linspace(3, 4, n=101))
 tbl$registered <- predict(fit, node="registered", data=tbl, method="bayes-lw")
 tbl
 
-# predict users for different gasprices
-tbl <- data.frame(gasprice=linspace(min(data$gasprice), max(data$gasprice), n=100))
-tbl$casual <- predict(fit, node="casual", data=tbl, method="bayes-lw")
-tbl$registered <- predict(fit, node="registered", data=tbl, method="bayes-lw")
-tbl
-
+# predict casual users for events
 tbl <- expand.grid(rallyprotest=c(1, 0))
 tbl$casual <- predict(fit, node="casual", data=tbl, method="bayes-lw")
-tbl$registered <- predict(fit, node="registered", data=tbl, method="bayes-lw")
 tbl
 
 
-vars = c(
-    "instant", "gasprice", "holiday", "workingday", "atemp",
-    "rallyprotest", "daylength"
-)
-for (V in vars)
-{
-    if (abs(pcor(data[c(V, "casual")])$estimate[2]) > 0.25)
-    {
-        print(paste("casual:", V))
-    }
-    if (abs(pcor(data[c(V, "registered")])$estimate[2]) > 0.25)
-    {
-        print(paste("registered:", V))
-    }
-}
-
-
+# predict users given all other variables from 2011 to 2013
+# first make correct dataframe for 2013, except the weather variables from 2011
 year3 <- data.frame(
-    instant=732:1096,
-    season=data[1:365, "season"],
+    day=732:1096,
+    season=data[1:365, "season"] == 1,
     holiday=1:365 %in% c(1, 21, 49, 106, 147, 185, 245, 287, 315, 332, 359),
     rallyprotest=1:365 %in% c(26, 48, 250, 286),
-    workingday=data[4:368, "workingday"],
+    workingday=data[4:368, "workingday"] == 1,
     daylength=daylength(732:1096),
     gasprice=-1,
     hum=data[1:365, "hum"],
@@ -226,6 +218,8 @@ year3 <- data.frame(
     temp=data[1:365, "temp"],
     atemp=data[1:365, "atemp"]
 )
+
+# add weekly gas prices to the 2013 dataframe
 gasprice.per.week <- c(
     3.5, 3.519, 3.513, 3.489, 3.525, 3.665, 3.73, 3.776, 3.806, 3.774, 3.746,
     3.711, 3.694, 3.67, 3.629, 3.58, 3.52, 3.498, 3.49, 3.522, 3.538, 3.557,
@@ -234,27 +228,45 @@ gasprice.per.week <- c(
     3.336, 3.295, 3.341, 3.423, 3.441, 3.447, 3.457, 3.466, 3.511
 )
 week.starts <- c(1, (1:365)[(hour.data[day.starts, "weekday"] == 5)[1:365]], 366)
-for (I in 1:(length(week.starts) - 1))
+for (week in 1:(length(week.starts) - 1))
 {
-  year3[week.starts[I]:(week.starts[I + 1] - 1), "gasprice"] <- gasprice.per.week[I]
+  year3[week.starts[week]:(week.starts[week + 1] - 1), "gasprice"] <- gasprice.per.week[week]
 }
+
+# put ordinal weather situation in 2013 dataframe
 numeric.year3 <- data.frame(lapply(year3, as.numeric))
-year3$weathersit <-data[1:365, "weathersit"]
+year3 <- cbind(numeric.year3, weathersit=data[1:365, "weathersit"])
 
-years <- data.frame(instant=1:1096)
-years$casual <- predict(fit, node="casual", data=rbind(subset(data, select=-c(casual, registered)), year3), method="bayes-lw")
-years$registered <- predict(fit, node="registered", data=rbind(subset(data, select=-c(casual, registered)), year3), method="bayes-lw")
+# combine data frames and predict casual and registered users for all data
+years <- rbind(subset(data, select=-c(casual, registered)), year3)
+years$casual <- predict(fit, node="casual", data=years, method="bayes-lw")
+years$registered <- predict(fit, node="registered", data=years, method="bayes-lw")
 
-ggplot(years) +
-   geom_point(aes(x=instant, y=casual)) +
-   xlab("days") + ylab("Number of casual users")
-ggplot(years) +
-   geom_point(aes(x=instant, y=registered)) +
-   xlab("days") + ylab("Number of registered users")
-ggplot(years) +
-   geom_point(aes(x=instant, y=casual + registered)) +
-   xlab("days") + ylab("Total number of users")
-ggplot(data) +
-    geom_point(aes(x=instant, y=casual + registered)) +
-    xlab("days") + ylab("Total number of users")
-                                        
+# compare predictions to true numbers of users
+ggarrange(
+    ggplot(data) +
+        geom_point(aes(x=day, y=casual)) +
+        xlab("days") + ylab("True number of casual users"),
+    ggplot(years) +
+        geom_point(aes(x=day, y=casual)) +
+        xlab("days") + ylab("Predicted number of casual users"),
+    nrow=2
+)
+ggarrange(
+    ggplot(data) +
+        geom_point(aes(x=day, y=registered)) +
+        xlab("days") + ylab("True number of registered users"),
+    ggplot(years) +
+        geom_point(aes(x=day, y=registered)) +
+        xlab("days") + ylab("Predicted number of registered users"),
+    nrow=2
+)
+ggarrange(
+  ggplot(data) +
+    geom_point(aes(x=day, y=casual + registered)) +
+    xlab("days") + ylab("True total number of users"),
+  ggplot(years) +
+    geom_point(aes(x=day, y=casual + registered)) +
+    xlab("days") + ylab("Predicted total number of users"),
+  nrow=2
+)
